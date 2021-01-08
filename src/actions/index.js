9cfe235e9f71bb18394f4e85f186ee06eb0e4603
Aleksandr Zhuravlev <ja@idaproject.com>
2021-01-08 20:41:23 +0300

    исправил форматирование

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,15 +22,15 @@ const bookAddedToCart = (bookId) => {
   return {
     type: 'BOOK_ADDED_TO_CART',
     payload: bookId
-  }
-}
+  };
+};
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
   dispatch(booksRequested());
   bookstoreService.getBooks()
     .then((data) => dispatch(booksLoaded(data)))
     .catch((err) => dispatch(booksError(err)));
-}
+};
 
 export {
   fetchBooks,
